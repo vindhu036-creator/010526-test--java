ce2eb55ffcbdff01b1f9dfda93614b8cfe86b0d3
SwajyotTech <info@swajyot.co.in>
2026-05-02 10:55:48 +0530

    local config changes

---
+++ src/main/java/com/testing/HomeController.java
@@ -0,0 +1,13 @@
+package com.testing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // this maps to index.html
+    }
+}
